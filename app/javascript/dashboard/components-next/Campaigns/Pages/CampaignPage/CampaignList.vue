4e47a5cd6682b78e87514b7218bf5421184b766e
Coidsa <yaqut@coid.sa>
2025-11-21 00:27:47 +0300

    Add statistics prop to CampaignList component

--- app/javascript/dashboard/components-next/Campaigns/Pages/CampaignPage/CampaignList.vue
+++ app/javascript/dashboard/components-next/Campaigns/Pages/CampaignPage/CampaignList.vue
@@ -30,6 +30,7 @@ const handleDelete = campaign => emit('delete', campaign);
       :sender="campaign.sender"
       :inbox="campaign.inbox"
       :scheduled-at="campaign.scheduled_at"
+      :statistics="campaign.statistics"
       :is-live-chat-type="isLiveChatType"
       @edit="handleEdit(campaign)"
       @delete="handleDelete(campaign)"
